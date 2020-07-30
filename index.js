77075a66c1071e9ae3016e3a9871e18e49b85101
Josh <josh1234king@gmail.com>
2020-07-30 13:34:22 +0200

    Added some changes on my contact form

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ function validation () {
     var email = document.getElementById("email").value;
     var message = document.getElementById("msg").value;
 
-
     error_message.style.padding = "10px";
     var text;
 
     if(name.length < 5){
-        text = "Please enter valid name";
+        text = "Please enter a valid name";
         error_message.innerHTML = text;
         return false;
     }
 
     if(email.indexOf("@") == -1 || email.length < 6){
-        text = "Please Enter valid Email";
+        text = "Please enter a valid email";
         error_message.innerHTML = text;
         return false;
     }
@@ -23,10 +22,10 @@ function validation () {
         text = "Please enter more than 5 characters";
         error_message.innerHTML = text;
         return false;
-      }
-    else 
-    text= "Successfully submitted your message";
+    }
+    else{ 
+    text= "Thank you for Your feedback,we'll reach out to you soon!";
     error_message.innerHTML = text;
     return true;
+    }
 }
-
